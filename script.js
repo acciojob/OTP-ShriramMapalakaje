0c50ed2e8191b8a504c2f5fcd2ca9bc333b064e1
ShriramMapalakaje <108290635+ShriramMapalakaje@users.noreply.github.com>
2026-02-05 13:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ codes[0].focus();
 
 codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
-        if (e.key >= 0 && e.key <= 9) {
-            codes[idx].value = ''; // Clear current value
-            setTimeout(() => codes[idx + 1]?.focus(), 10);
+        if (e.key >= '0' && e.key <= '9') {
+            // Force value to be empty before typing a new one
+            codes[idx].value = ''; 
+            setTimeout(() => {
+                if (idx < codes.length - 1) {
+                    codes[idx + 1].focus();
+                }
+            }, 10);
         } else if (e.key === 'Backspace') {
-            setTimeout(() => codes[idx - 1]?.focus(), 10);
+            // If backspace is pressed, clear current and move focus back
+            setTimeout(() => {
+                if (idx > 0) {
+                    codes[idx - 1].focus();
+                }
+            }, 10);
         }
     });
 });

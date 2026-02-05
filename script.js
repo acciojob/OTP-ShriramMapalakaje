b26981d8d0006ab9f4222b1e05ce92708f6a7887
ShriramMapalakaje <108290635+ShriramMapalakaje@users.noreply.github.com>
2026-02-05 13:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-//your JS code here. If required.
 const codes = document.querySelectorAll('.code');
 
-
+// Focus first input on load
 codes[0].focus();
 
 codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
         if (e.key >= 0 && e.key <= 9) {
-            
-            codes[idx].value = '';
-           
-            setTimeout(() => {
-                if (idx < codes.length - 1) {
-                    codes[idx + 1].focus();
-                }
-            }, 10);
+            codes[idx].value = ''; // Clear current value
+            setTimeout(() => codes[idx + 1]?.focus(), 10);
         } else if (e.key === 'Backspace') {
-            
-            setTimeout(() => {
-                if (idx > 0) {
-                    codes[idx - 1].focus();
-                }
-            }, 10);
+            setTimeout(() => codes[idx - 1]?.focus(), 10);
         }
     });
 });
